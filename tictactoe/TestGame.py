414eb3abb707a37a00be8ec4691dd6fcadc3e086
Warren MacEvoy <wmacevoy@gmail.com>
2018-01-25 02:02:21 -0700

    refactored test

--- tictactoe/TestGame.py
+++ tictactoe/TestGame.py
@@ -1,24 +1,24 @@
 import unittest
 from Const import Const
-from State import State
+from Game import Game
 
 class TestState(unittest.TestCase):
     def testConstruct(self):
-        unit = State()
+        unit = Game()
         self.assertEqual(unit.getState(),Const.STATE_TURN_O)
 
     def testXWin(self):
-        unit = State()
+        unit = Game()
         unit.play("oa2 xa1 oa3 xb1 ob2 xc1")
         self.assertEqual(unit.getState(),Const.STATE_WIN_X)
 
     def testOWin(self):
-        unit = State()
+        unit = Game()
         unit.play("oc1 xa2 oa1 xa3 ob1")
         self.assertEqual(unit.getState(),Const.STATE_WIN_O)
 
     def testDraw(self):
-        unit = State()
+        unit = Game()
         unit.play("oa1 xa2 ob2 xc3 oc2 xa3 ob3 xb1 oc1")
         self.assertEqual(unit.getState(),Const.STATE_DRAW)
 if __name__ == '__main__':
