8c194905919c07a57d3e3b799c9e3db430fe363f
Warren MacEvoy <wmacevoy@gmail.com>
2018-01-25 02:00:14 -0700

    rename state to game and add moves method

--- tictactoe/Game.py
+++ tictactoe/Game.py
@@ -1,7 +1,13 @@
 from Const import Const
 from Move import Move
 
-class State:
+class Game:
+    def over(self):
+        return \
+            self._state == Const.STATE_WIN_O or \
+            self._state == Const.STATE_WIN_X or \
+            self._state == Const.STATE_DRAW
+
     def moveOk(self,row,col,mark):
         Const.rowOk(row)
         Const.colOk(col)
@@ -12,7 +18,25 @@ class State:
         if (self._state == Const.STATE_TURN_X) and (mark == Const.MARK_X) and \
            self._board[row][col] == Const.MARK_NONE:
            return
-        raise ValueError("move invalid in current state")
+        raise ValueError("move (" + str(Move(mark,row,col)) + ") invalid in current state")
+
+    def getMoves(self):
+        mark = None
+        if self._state == Const.STATE_TURN_O:
+            mark = Const.MARK_O
+        elif self._state == Const.STATE_TURN_X:
+            mark = Const.MARK_X
+
+        if mark == None:
+            return []
+
+        moves = []
+        for row in range(Const.ROWS):
+            for col in range(Const.COLS):
+                if self._board[row][col] == Const.MARK_NONE:
+                    moves.append(Move(row,col,mark))
+
+        return moves
 
     def _repeats(self,row,col,rowDir,colDir):
         mark = self._board[row][col]        
@@ -61,17 +85,19 @@ class State:
                 self._state = Const.STATE_TURN_O
 
     def unmove(self,row,col):
-        State.rowOk(row)
-        State.colOk(col)
+        Const.rowOk(row)
+        Const.colOk(col)
         if self._board[row][col] == Const.MARK_X:
             self._unplayed = self._unplayed + 1
             self._board[row][col] = Const.MARK_NONE
             self._state = Const.STATE_TURN_X
-        if self._board[row][col] == Const.MARK_O:
+        elif self._board[row][col] == Const.MARK_O:
             self._unplayed = self._unplayed + 1
             self._board[row][col] = Const.MARK_NONE
             self._state = Const.STATE_TURN_O
-            
+        else:
+            raise ValueError("unmove (" + str(row) + "," + str(col) + ") invalid in current state")
+
     def reset(self):
         self._board = [[Const.MARK_NONE for col in range(Const.COLS)] for row in range(Const.ROWS)]
 
