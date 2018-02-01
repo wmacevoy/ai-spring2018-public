064dcb648ed7b582c082df773d1117e0829e1e6e
Warren MacEvoy <wmacevoy@gmail.com>
2018-02-01 12:09:12 -0700

    heuristics

--- tictactoe/MinMaxAgent.py
+++ tictactoe/MinMaxAgent.py
@@ -10,7 +10,9 @@ class MinMaxAgent:
             raise ValueError("side must be MARK_X or MARK_O")
         self._side = side
         self._valueCache = {}
+        self._cached = 0
 
+        
     def uncachedValue(self,game):
         ans = None
         state = game.getState()
@@ -36,6 +38,8 @@ class MinMaxAgent:
         myTurn = (iside == iturn)
         myOptions = 0
 
+#        if depth == self._maxDepth:
+#             return self.heuristicValue(game)
         for move in game.getMoves():
             move.play(game)
             (moveValue,moveOptions)=self.value(game)
@@ -51,9 +55,33 @@ class MinMaxAgent:
 
         return (ans,myOptions)
 
+    def heuristicValue(self,game):
+        if game.over(): return self.uncachedValue(game)[0]
+        xscore = 0
+        oscore = 0
+        for row in range(Const.ROWS):
+            for col in range(Const.COLS):
+                if game._board[row][col] == Const.MARK_NONE:
+                  for drow in range(-1,2):
+                      for dcol in range(-1,2):
+                          nbrRow = row + drow
+                          nbrCol = col + dcol
+                          if (nbrRow >= 0 and nbrRow < Const.ROWS):
+                              if (nbrCol >= 0 and nbrCol < Const.COLS):
+                                  if game._board[nbrRow][nbrCol] == Const.MARK_X:
+                                      xscore = xscore + 1
+                                  if game._board[nbrRow][nbrCol] == Const.MARK_O:
+                                      oscore = oscore + 1
+        if xscore == oscore: return 0
+        if self._side == Const.MARK_X:
+            return float(xscore-oscore)/float(xscore+oscore)
+        else:
+            return float(oscore-xscore)/float(xscore+oscore)            
+                                    
     def value(self,game):
-        index=game.getIndex()
-        if index in self._valueCache: return self._valueCache[index]
+        for equivGame in game.getEquivClass():
+            index = equivGame.getIndex()
+            if index in self._valueCache: return self._valueCache[index]
         ans = self.uncachedValue(game)
         self._valueCache[index]=ans
         return ans
